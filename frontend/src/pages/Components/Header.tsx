4722b048081df997eba3e1dd84dca72236a0efaf
Stefan-Natov <stefanaki1512@gmail.com>
2026-03-26 18:14:17 +0200

    fixed padding

--- frontend/src/pages/Components/Header.tsx
+++ frontend/src/pages/Components/Header.tsx
@@ -1,6 +1,4 @@
-﻿import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMap } from "@fortawesome/free-solid-svg-icons";
+﻿
 import { useNavigate } from "react-router-dom";
 import "../Header.css";
 
