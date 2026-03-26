adc0892d3e739978f066d1dd1589a56f51bba88b
minidps <dslavov2010@gmail.com>
2026-03-26 17:17:28 +0200

    stuff

--- frontend/src/pages/Components/Header.tsx
+++ frontend/src/pages/Components/Header.tsx
@@ -2,6 +2,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMap } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
+import logoImg from "../../images/pulsepoint.png";
 import "../Header.css";
 
 export default function Header() {
@@ -12,7 +13,7 @@ export default function Header() {
     <header className="mp-header">
       <div className="mp-logo">
         <div className="mp-logo-icon">
-          <FontAwesomeIcon icon={faMap} />
+          <img src={logoImg} alt="PulsePoint Logo" className="mp-logo-img" />
         </div>
         <span className="mp-logo-text">PulsePoint</span>
       </div>
