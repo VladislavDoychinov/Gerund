d926e1bd2d2daab9a8d1af3de98fbfbfa8a121df
Stefan-Natov <stefanaki1512@gmail.com>
2026-03-26 18:55:47 +0200

    mergin yayyy

--- frontend/src/pages/Components/Header.tsx
+++ frontend/src/pages/Components/Header.tsx
@@ -1,4 +1,4 @@
-﻿import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import "../Header.css";
 
 export default function Header() {
