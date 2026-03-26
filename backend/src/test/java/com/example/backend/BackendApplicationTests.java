0b3bc0b85a5170ddb5ca37beabe19b855b3a7276
VladislavDoychinov <vladi.velinov.doychinov@gmail.com>
2026-03-26 10:52:10 +0200

    clean

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.asp5640.fullstack_backend;
+package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FullstackBackendApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
