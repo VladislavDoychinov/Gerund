9bec57b9b702d075e363735f32c2d3a3639eb6c7
VladislavDoychinov <vladi.velinov.doychinov@gmail.com>
2026-03-26 14:37:38 +0200

    pushing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,12 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-<<<<<<< HEAD
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Main from "./pages/Main";
 import StorePage from "./pages/StorePage";
 import ProductPage from "./pages/ProductPage";
 import Profile from "./pages/Profile";
-=======
-import Register from "./Pages/Register";
-import Login from "./Pages/Login";
-import Main from "./Pages/Main";
-import StorePage from "./Pages/StorePage";
-import ProductPage from "./Pages/ProductPage";
-import Profile from "./Pages/Profile";
-import AddProduct from "./Pages/AddProduct";
->>>>>>> 28d98c3e9e54276cdaab043dafffdcf62df70f0b
+import AddProduct from "./pages/AddProduct";
 
 function App() {
   return (
