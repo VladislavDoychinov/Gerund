97e6e7f57a050559966f15cd23fa0598bd594e72
ImpairedAsp5640 <impairedasp5640@gmail.com>
2026-03-26 13:14:12 +0200

    app.tsx brought back

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Main from "./pages/Main";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/main" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
