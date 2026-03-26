ffc4ec0c2f64765749ca6fab647959bb277e819c
ImpairedAsp5640 <impairedasp5640@gmail.com>
2026-03-26 10:15:35 +0200

    backend

--- backend/src/main/java/com/asp5640/fullstack_backend/FullstackBackendApplication.java
+++ backend/src/main/java/com/asp5640/fullstack_backend/FullstackBackendApplication.java
@@ -1,13 +1,13 @@
-package com.example.backend;
+package com.asp5640.fullstack_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class FullstackBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(FullstackBackendApplication.class, args);
 	}
 
 }
