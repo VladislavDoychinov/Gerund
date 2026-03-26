3ca83314f9786465142fcbbd89ab5b62396e2cd5
minidps <dslavov2010@gmail.com>
2026-03-26 13:22:26 +0200

    Strawberries

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,9 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Main from "./pages/Main";
-<<<<<<< HEAD
 import StorePage from "./pages/StorePage";
 import ProductPage from "./pages/ProductPage";
-=======
 import Profile from "./pages/Profile";
->>>>>>> 9fbdde90c88017aaff1c0c96bcfcba8cf0fade31
 
 function App() {
   return (
@@ -17,12 +14,8 @@ function App() {
         <Route path="/signup" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/main" element={<Main />} />
-<<<<<<< HEAD
         <Route path="/store" element={<StorePage />} />
         <Route path="/product/:id" element={<ProductPage />} />
-=======
-        <Route path="/profile" element={<Profile />} />
->>>>>>> 9fbdde90c88017aaff1c0c96bcfcba8cf0fade31
       </Routes>
     </BrowserRouter>
   );
