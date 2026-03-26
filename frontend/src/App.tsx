4b53602becc03e3a02f6ce58b78b9fe0aa70863f
ImpairedAsp5640 <impairedasp5640@gmail.com>
2026-03-26 14:53:52 +0200

    login fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-<<<<<<< HEAD
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Main from "./pages/Main";
-import StorePage from "./pages/StorePage";
-import ProductPage from "./pages/ProductPage";
-import Profile from "./pages/Profile";
-=======
+
 import Register from "./Pages/Register";
 import Login from "./Pages/Login";
 import Main from "./Pages/Main";
@@ -15,7 +8,6 @@ import StorePage from "./Pages/StorePage";
 import ProductPage from "./Pages/ProductPage";
 import Profile from "./Pages/Profile";
 import AddProduct from "./Pages/AddProduct";
->>>>>>> 28d98c3e9e54276cdaab043dafffdcf62df70f0b
 
 function App() {
   return (
